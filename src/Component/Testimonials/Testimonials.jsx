91e75b53144cc7d3c9728001d5f9bfc9f060ab11
coders rabbi <pcrabbi2020@gmail.com>
2023-06-22 17:23:32 +0600

    tastimonial image round full

---
+++ src/Component/Testimonials/Testimonials.jsx
@@ -0,0 +1,50 @@
+import img1 from "../../assets/tastimonial/testimonial-img1.jpg"
+
+const Testimonials = () => {
+    return (
+        <div>
+            <h3 className='text-[#FF3333]'>Testimonials</h3>
+            <h1 className='text-'>What People and Clients Think About Us?</h1>
+            <div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src={img1}  alt="Shoes" className="xl rounded-full" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonials;
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeBanner from './HomeBanner';
 import Tracking from '../../Component/Tracking/Tracking';
+import Testimonials from '../../Component/Testimonials/Testimonials';
 
 const Home = () => {
     return (
         <div>
             <HomeBanner></HomeBanner>
             <Tracking></Tracking>
+            <Testimonials></Testimonials>
         </div>
     );
 };
