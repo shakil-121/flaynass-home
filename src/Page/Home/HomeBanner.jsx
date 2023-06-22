73f6c80a4b176ae82ee0e4902ad880fb923533cc
coders rabbi <pcrabbi2020@gmail.com>
2023-06-22 15:34:25 +0600

    imge added

--- src/Page/Home/HomeBanner.jsx
+++ src/Page/Home/HomeBanner.jsx
@@ -1,14 +1,19 @@
 import bannerAnimation from "../../assets/banner/Delivery BAKE новая попытка.json"
 import Lottie from "lottie-react";
+import './../Home/HomeBanner.css'
 
 const HomeBanner = () => {
     return (
-        <div className='max-w-screen-xl mx-auto'>
-            <div className="">
-                <h1>Express <span>Home Delivery</span></h1>
-            </div>
-            <div className="w-96">
-                <Lottie animationData={bannerAnimation} loop={true} />;
+        <div className='.rabbi'>
+            <div className="max-w-screen-xl mx-auto grid md:grid-cols-2 justify-items-center py-72">
+                <div className="text-white">
+                    <h1 className="text-[80px] font-bold">Express <br /><span className="text-[#FF3333]">Home Delivery</span></h1>
+                    <p className="">Curabitur imperdiet varius lacus, id placerat purus vulputate non. Fusce in <br /> felis vel arcu maximus placerat eu ut arcu. Ut nunc ex, gravida vel porttitor <br /> et, pretium ac sapien.</p>
+                    <button className="btn btn-secondary">Button</button>
+                </div>
+                <div className="w-96">
+                    <Lottie animationData={bannerAnimation} loop={true} />;
+                </div>
             </div>
         </div>
     );
