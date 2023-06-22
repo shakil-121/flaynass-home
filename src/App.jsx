444a9646583af3758b7dee744728616442e36f72
shakil-121 <shahadatshakil76@gmail.com>
2023-06-22 13:11:17 +0600

    router done

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,3 @@ function App() {
 }
 
 export default App
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/shakil-121/flaynass-home.git
-// git push -u origin main
